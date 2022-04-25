c7b19c659416c389a7f827fcf677100d42a73a92
Brithnie Estime <estimebrithnie@yahoo.com>
2022-04-24 22:53:53 -0700

    created (1each)function to allow users in selcting to add and update employee info.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Import and require inquirer, express, and mysql2
+// Import and require 
 const inquirer = require('inquirer');
 // const mysql = require('mysql2');
 const connection = require('./config/connection');
@@ -37,7 +37,7 @@ function init() {
             })
 }
 
-// The view function
+// Function to allow user to view the tables' contents
 function viewFunc() {
     inquirer
     .prompt([
@@ -67,7 +67,9 @@ function viewFunc() {
 function departments() {
     connection.query("SELECT * FROM department", function(err, res) {
         if (err) throw err;
+        console.log('---◆----------------------◆◆◆----------------------◆---');
         console.log(res);
+        console.log('---◆----------------------◆◆◆----------------------◆---');
         init();
     })
 }
@@ -75,27 +77,93 @@ function departments() {
 function roles(){
     connection.query("SELECT * FROM role", function(err, res) {
         if (err) throw err;
+        console.log('---◆----------------------◆◆◆----------------------◆---');
         console.log(res);
+        console.log('---◆----------------------◆◆◆----------------------◆---');
         init();
     })
 }
 
 function employees() {
-    connection.query("SELECT * FROM employee INNER JOIN role ON role_id INNER JOIN department ON department_id", function(err, res) {
+    connection.query("SELECT * FROM employee", function(err, res) {
         if (err) throw err;
+        console.log('---◆----------------------◆◆◆----------------------◆---');
         console.log(res);
-        init();    
+        console.log('---◆----------------------◆◆◆----------------------◆---');
+        init();
     })
 }
 
-// // Add Function
-// function addFunc() {
+// Function to allow user to create tables' contents
+function addFunc() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'name',
+            choices: ["Add department", "Add role", "Add employee", "Exit App"],
+            message: 'Select the table you would like to add to',
+        }]).then(function (response) {
+            switch (response.name) {
+                case "Add department":
+                    addToDepartment();
+                    break;
+                case "Add role":
+                    addToRole();
+                    break;
+                case "Add employee":
+                    addToEmployee();
+                    break;
+                default:
+                    console.log("----------- You've exited the app successfully! -----------");
+            }
+            init();
+        })
+}
 
-// }
+function addToDepartment() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: 'Please input the department name you would like to add.'
+        }]).then(function (response) {
+            connection.query("INSERT INTO department VALUES (?)", [response.department], function(err) {
+                if (err) throw err;
+                console.log('---◆----------------------◆◆◆----------------------◆---');
+                console.log('Added successfully!' + response.department);
+                console.log('---◆----------------------◆◆◆----------------------◆---');
+                init();
+            })
+        })
+}
 
-// // Update Function
-// function updateFunc() {
-    
-// }
+// Function to allow user to update the tables' contents
+function updateFunc() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'name',
+            choices: ["Update department", "Update role", "Update employee", "Exit App"],
+            message: 'Select the table you would like to update',
+        }]).then(function (response) {
+            switch (response.name) {
+                case "Update department":
+                    updateDepartment();
+                    break;
+                case "Update role":
+                    updateRole();
+                    break;
+                case "Update employee":
+                    updateEmployee();
+                    break;
+                default:
+                    console.log("----------- You've exited the app successfully! -----------");
+            }
+            init();
+        })
+}
 
 init();
